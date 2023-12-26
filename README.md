39ea2488ae84e9cbd55ae83e00feebf0daf8f9f7
Haoming <hmstudy02@gmail.com>
2023-12-26 13:41:15 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# YouTube Audio Normalizer
+A simple **Chromium** extension that attempts to balance the volume of YouTube videos
+
+## How to Install
+0. Clone this repo
+1. Open the **Manage extensions** page of your browser (**eg.**`edge://extensions/`)
+2. Enable **Developer mode**
+3. Click **Load unpacked** button
+4. Select the folder of this repo
+5. It should now show up on the list of **Installed extensions**
+
+## How to Use
+- When watching a video on YouTube, if you think the volume is too low or too high,
+simply right click the video, and click the **Stats for nerds** button. The volume should then be normalized.
---
+++ content.js
@@ -0,0 +1,62 @@
+class AudioNormalizer {
+
+    static CachedURL = "InsertRickRoll";
+    static Instance = null;
+    static gainNode = null;
+
+    static initialize() {
+        const audioCtx = new AudioContext();
+
+        this.gainNode = audioCtx.createGain();
+        this.gainNode.connect(audioCtx.destination);
+
+        const videoElement = document.querySelector('video');
+        const source = audioCtx.createMediaElementSource(videoElement);
+
+        source.connect(this.gainNode);
+    }
+
+    static getVolume(panel) {
+        const volumeLabel = panel.childNodes[1].childNodes[3].innerHTML;
+        const volume = parseFloat(volumeLabel.split('loudness')[1].split('dB')[0]);
+
+        return Math.pow(10, -volume / 20);
+    }
+
+    constructor(url) {
+        this.succeeded = false;
+
+        AudioNormalizer.CachedURL = url;
+        if (AudioNormalizer.gainNode === null)
+            AudioNormalizer.initialize();
+
+        AudioNormalizer.gainNode.gain.value = 1.0;
+    }
+
+    tryNormalize() {
+        if (this.succeeded)
+            return;
+
+        const panel = document.querySelector('.html5-video-info-panel');
+
+        if (panel === null)
+            return;
+
+        const increase = AudioNormalizer.getVolume(panel);
+        AudioNormalizer.gainNode.gain.value = increase;
+
+        console.log('Normalized Volume by ' + increase.toFixed(2) + ' dB');
+
+        this.succeeded = true;
+    }
+
+}
+
+document.addEventListener('click', () => {
+
+    if (AudioNormalizer.CachedURL !== window.location.href)
+        AudioNormalizer.Instance = new AudioNormalizer(window.location.href);
+
+    AudioNormalizer.Instance.tryNormalize();
+
+});
